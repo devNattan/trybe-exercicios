c2132f9a651072b6f8a83851124cc3fd6df7c4cc
devNattan <natanaelhenrique.s.c@gmail.com>
2022-10-24 15:05:28 -0300

    finalizando exercicio 5 FOGUETE

---
+++ fundamentos/secao-04-introducao-ao-js/dia-01-primeiros-passos/exercicio-05-foguete.js
@@ -0,0 +1,14 @@
+let ang1 = 60;
+let ang2 = 60;
+let ang3 = 60;
+
+if (ang1 && ang2 && ang3 > 0) {
+    console.log('Todos os números são positivos!!!')
+    if (ang1 + ang2 + ang3 === 180) {
+        console.log('true')
+    } else {
+        console.log('false')
+    }
+} else {
+    console.log('ERROR')
+}
