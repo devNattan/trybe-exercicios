1ad85d3eb42ba6b69d6febad52e222922be2066d
devNattan <natanaelhenrique.s.c@gmail.com>
2022-10-25 11:46:24 -0300

    Finalizando o FOGUETEgit add .!

--- fundamentos/secao-04-introducao-ao-js/dia-02-array-e-loop-for/exercicio-01.js
+++ fundamentos/secao-04-introducao-ao-js/dia-02-array-e-loop-for/exercicio-01.js
@@ -29,6 +29,8 @@ let numbers = [5,9,3,19,70,8,100,2,35,27];
 //     console.log('Número menor que 20!!')
 // }
 
+// CHECANDO O MAIOR NÚMERO
+
 // let maiorNumero = numbers[0]
 
 // for (let i= 1; i< numbers.length; i++) {
@@ -38,3 +40,34 @@ let numbers = [5,9,3,19,70,8,100,2,35,27];
 // }
 
 // console.log(maiorNumero)
+
+// CHECANDO OS VALORES ÍPARES
+
+// for (let i= 1; i< numbers.length; i++) {
+//     if (numbers[i] % 2 != 0) {
+//         console.log(numbers[i])
+//     } else {
+//         console.log('Nenhum valor encontrado!')
+//     }
+// }
+
+// CHECANDO O MENOR VALOR
+
+// let menorNumero = numbers[0]
+
+// for (let i= 1; i< numbers.length; i++) {
+//     if (numbers[i] < menorNumero) {
+//         menorNumero = numbers[i]
+//     }
+// }
+// console.log(menorNumero)
+
+// Imprimindo do 1 ao 25 com for
+
+// let numbers2 = [];
+
+// for (let i = 1; i <= 25; i ++) {
+//   numbers2.push(i);
+// }
+
+// console.log(numbers2);
