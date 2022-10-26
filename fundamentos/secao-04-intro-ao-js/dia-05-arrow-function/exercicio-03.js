e3e7772dc91ebd89e1daa8f0cf06eacb29002c64
devNattan <natanaelhenrique.s.c@gmail.com>
2022-10-26 18:23:22 -0300

    exercicio 3

---
+++ fundamentos/secao-04-intro-ao-js/dia-05-arrow-function/exercicio-03.js
@@ -0,0 +1,9 @@
+// function hello(nome) {
+//     return `Olá, ${nome}!`
+//   }
+//   let nome = 'Ivan';
+//   console.log(hello(nome));
+
+  const retornaNome = (nome) => `Olá, ${nome}!`;
+
+  console.log(retornaNome('Ivan'));
