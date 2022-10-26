f9476a1bf6a00c8682d10ddbf0dffa4868046bdb
devNattan <natanaelhenrique.s.c@gmail.com>
2022-10-26 18:26:06 -0300

    exercicio 4

---
+++ fundamentos/secao-04-intro-ao-js/dia-05-arrow-function/exercicio-04.js
@@ -0,0 +1,10 @@
+// function nomeCompleto(nome, sobrenome) {
+//     return `${nome} ${sobrenome}`
+//   }
+//   let nome = 'Ivan';
+//   let sobrenome = 'Pires';
+//   console.log(nomeCompleto(nome, sobrenome));
+
+  const retornaNome = (nome, sobrenome) => `${nome} ${sobrenome}`;
+
+  console.log(retornaNome('Ivan','Pires'));
