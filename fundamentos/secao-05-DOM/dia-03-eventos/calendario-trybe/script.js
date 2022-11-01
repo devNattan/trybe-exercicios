5937a8eb371849f8f645f2419cfc8a401dea4938
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-01 19:17:35 -0300

    finalizando passo 2

--- fundamentos/secao-05-DOM/dia-03-eventos/calendario-trybe/script.js
+++ fundamentos/secao-05-DOM/dia-03-eventos/calendario-trybe/script.js
@@ -14,6 +14,8 @@ const createDaysOfTheWeek = () => {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
+
+// adicionando dias no calendário de forma dinâmica
   const addDays = () => {
     const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
     const days = document.querySelector('#days');
@@ -42,10 +44,14 @@ const createDaysOfTheWeek = () => {
 
   addDays()
 
-  // for (i = 0; i < decemberDaysList.length; i++) {
-    //     let dia = decemberDaysList[i];
-    //     let criaDiaLi = document.createElement('li')
-    //     criaDiaLi.innerHTML = dia;
+// adicionando botão dinamicamente
+let criaBotao = (string) => {
+    const containerDeBotoes = document.querySelector('.buttons-container');
+    let botao = document.createElement('button');
+    botao.innerHTML = string;
+    botao.id = 'btn-holiday'
+    containerDeBotoes.appendChild(botao)
+}
 
-    //     days.appendChild(criaDiaLi)
-    // }
+criaBotao('Feriados')
+
