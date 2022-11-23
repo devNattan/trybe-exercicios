b14b0bf5b00cc216b0b094087ef67a2f6c50ac4d
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-23 08:36:00 -0300

    testando tudo da decode

--- my_new_project/mapString.test.js
+++ my_new_project/mapString.test.js
@@ -8,6 +8,15 @@ describe('testa se as funções encode e decode funcionam', () => {
         expect(typeof encode).toEqual('function');
     });
     test('testa se encode retorna o resultado esperado', () => {
-        expect(encode('aeiou')).toEqual('12345')
+        expect(encode('aeiou')).toEqual('12345');
+    });
+    test('se decode é definida', () => {
+        expect(decode).toBeDefined();
+    });
+    test('testa se decode é uma func', () => {
+        expect(typeof decode).toEqual('function');
+    });
+    test('testa se decode retorna o resultado esperado', () => {
+        expect(decode('12345')).toEqual('aeiou');
     })
 });
