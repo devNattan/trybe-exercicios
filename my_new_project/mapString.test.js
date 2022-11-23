3e9b169bbba97c8b87d51d66b1ad18e6dee7188d
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-23 08:32:10 -0300

    testa se encode retorna o resultado esperado exercicio 3

--- my_new_project/mapString.test.js
+++ my_new_project/mapString.test.js
@@ -1,8 +1,13 @@
 const {encode, decode} = require('./mapString');
 
 describe('testa se as funções encode e decode funcionam', () => {
-    test('se encode é uma func', () => {
+    test('se encode é definida', () => {
         expect(encode).toBeDefined();
+    });
+    test('se encode é uma função', () => {
         expect(typeof encode).toEqual('function');
     });
+    test('testa se encode retorna o resultado esperado', () => {
+        expect(encode('aeiou')).toEqual('12345')
+    })
 });
