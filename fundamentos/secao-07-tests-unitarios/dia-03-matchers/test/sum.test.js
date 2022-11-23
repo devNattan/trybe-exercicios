c5056104940a202d8fcb0d089980b7d97b320383
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-23 16:29:48 -0300

    função sum blindada com sucesso

--- fundamentos/secao-07-tests-unitarios/dia-03-matchers/test/sum.test.js
+++ fundamentos/secao-07-tests-unitarios/dia-03-matchers/test/sum.test.js
@@ -1,2 +1,20 @@
 let sum = require('../src/sum');
 
+describe('testa a função sum', () =>{
+    it('testa se a função sum é declarada', () => {
+        expect(sum).toBeDefined();
+    });
+    it('testa se sum é uma função', () => {
+        expect(typeof sum).toEqual('function');
+    });
+    it('testa se o retorno de sum(4, 5) é 9', () => {
+        expect(sum(4,5)).toEqual(9);
+    });
+    it('testa se o retorno de sum(0, 0) é 0', () => {
+        expect(sum(0,0)).toEqual(0);
+    });
+    it('deve disparar um erro caso receba como parâmetro uma string', () => {
+        expect(() => sum(4,'5')).toThrowError();
+        expect(() => sum(4,'5')).toThrowError('parameters must be numbers');
+    });
+});
