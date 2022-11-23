8a319c27b096db263c7f0a85c3d433df9115146f
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-23 07:48:24 -0300

    realizando o primeiro e segundo test do exercicio

--- my_new_project/myRemove.test.js
+++ my_new_project/myRemove.test.js
@@ -1,1 +1,8 @@
 const myRemove = require('./myRemove');
+
+describe('testa a função myRemove', () => {
+    test('deve retornar um array removendo o item correto', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+        expect(myRemove([1,2,3,4], 3)).concat.toEqual([1,2,3,4]);
+    });
+});
