d6ed03c2197ecfb55ae39488ef761ad4fdfe0cf6
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-23 08:22:47 -0300

    testando se encode é uma func exercicio 3

--- my_new_project/mapString.test.js
+++ my_new_project/mapString.test.js
@@ -1,1 +1,8 @@
 const {encode, decode} = require('./mapString');
+
+describe('testa se as funções encode e decode funcionam', () => {
+    test('se encode é uma func', () => {
+        expect(encode).toBeDefined();
+        expect(typeof encode).toEqual('function');
+    });
+});
