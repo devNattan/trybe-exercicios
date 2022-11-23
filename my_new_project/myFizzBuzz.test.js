0347ed36ef3757780ad46412c4e572890e278d7a
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-23 08:09:58 -0300

    realizando o 3° test do exercicio 2

--- my_new_project/myFizzBuzz.test.js
+++ my_new_project/myFizzBuzz.test.js
@@ -4,5 +4,6 @@ describe('Testa a função fizzBuzz', () => {
     test('deve retornar o valor esperado', () => {
         expect(myFizzBuzz(15)).toBe('fizzbuzz');
         expect(myFizzBuzz(3)).toBe('fizz');
+        expect(myFizzBuzz(5)).toBe('buzz');
     });
 });
