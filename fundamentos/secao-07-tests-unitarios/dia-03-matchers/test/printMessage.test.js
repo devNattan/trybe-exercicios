dc1a625256c1b9b8e55af43d00b106c3badd0f62
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-23 16:41:16 -0300

    finalizando exercicio 3

--- fundamentos/secao-07-tests-unitarios/dia-03-matchers/test/printMessage.test.js
+++ fundamentos/secao-07-tests-unitarios/dia-03-matchers/test/printMessage.test.js
@@ -1,7 +1,19 @@
-const { info } = require('../src/printMessage');
+const { info, printMessage } = require('../src/printMessage');
 
 describe('testa se o obj info tem a chave personagem', () => {
     it('testa se info tem personagem dentro', () => {
         expect(info).toHaveProperty('personagem');
+    });
+});
+
+describe('testa se printMessage é uma função', () => {
+    it('testa se a função é definida', () => {
+        expect(printMessage).toBeDefined();
+    });
+    it('testa se a função é uma função', () => {
+        expect(typeof printMessage).toEqual('function');
+    });
+    it('testa se a função imprime', () => {
+        expect(printMessage(info)).toMatch('Boas vindas');
     });
 });
