9a9cbf22d6af9c2b93cf8e03829b02793d609923
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-29 15:30:40 -0300

    fazendo o segundo exercicio com o find

--- fundamentos/secao-08-HOFs/dia-01-introducao-ao-HOFs/para-fixar/find.js
+++ fundamentos/secao-08-HOFs/dia-01-introducao-ao-HOFs/para-fixar/find.js
@@ -3,5 +3,11 @@ const numbers = [19, 21, 30, 3, 45, 22, 15];
 // Adicione seu código aqui
 
 const verifyNumbers = numbers.find((number) => number % 3 === 0 && number % 5 === 0)
+// console.log(verifyNumbers);
 
-console.log(verifyNumbers);
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// Adicione seu código aqui
+
+const verifyNames = names.find((name) => name.length === 5)
+// console.log(verifyNames);
