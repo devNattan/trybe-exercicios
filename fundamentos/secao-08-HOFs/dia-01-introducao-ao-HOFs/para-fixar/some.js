2b17e73562ecdaf4e92e5036dd899f3d9ee625f6
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-29 15:54:31 -0300

    finalizando o para fixar com um exercicio delícia de HOFs usando o conceito de .every

--- fundamentos/secao-08-HOFs/dia-01-introducao-ao-HOFs/para-fixar/some.js
+++ fundamentos/secao-08-HOFs/dia-01-introducao-ao-HOFs/para-fixar/some.js
@@ -4,5 +4,21 @@ const hasName = (arr, name) => {
     return arr.some((currentName) => currentName === name); 
 };
 
-console.log(hasName(names, 'Ana'));
-console.log(hasName(names, 'Pedro'));
+// console.log(hasName(names, 'Ana'));
+// console.log(hasName(names, 'Pedro'));
+
+const people = [
+    { name: 'Mateus', age: 18 },
+    { name: 'José', age: 16 },
+    { name: 'Ana', age: 23 },
+    { name: 'Cláudia', age: 20 },
+    { name: 'Bruna', age: 19 },
+  ];
+  
+    //Adicione seu código aqui
+const verifyAges = (arr, minimumAge) => {
+    return arr.every((person) => person.age >= minimumAge);  
+};
+  
+  console.log(verifyAges(people, 18));
+  console.log(verifyAges(people, 14));
