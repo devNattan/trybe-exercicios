402aa4b496af7850dab42836e3bce876f7217a10
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-29 15:13:17 -0300

    criando a função do para fixar 1

---
+++ fundamentos/secao-08-HOFs/dia-01-introducao-ao-HOFs/para-fixar/script.js
@@ -0,0 +1,12 @@
+const emailListInData = [
+    'roberta@email.com',
+    'paulo@email.com',
+    'anaroberta@email.com',
+    'fabiano@email.com',
+  ];
+  
+  // Adicione seu código aqui
+
+  let cadastrado = (email) => {
+    console.log(`O email ${email} está cadastrado em nosso banco de dados!`);
+  };
