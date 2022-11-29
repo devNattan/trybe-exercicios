9fb850025d53cdebb1070f195b62deab327a5e71
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-29 15:34:25 -0300

    fazendo o terceiro exercicio com find

--- fundamentos/secao-08-HOFs/dia-01-introducao-ao-HOFs/para-fixar/find.js
+++ fundamentos/secao-08-HOFs/dia-01-introducao-ao-HOFs/para-fixar/find.js
@@ -11,3 +11,14 @@ const names = ['João', 'Irene', 'Fernando', 'Maria'];
 
 const verifyNames = names.find((name) => name.length === 5)
 // console.log(verifyNames);
+
+const musicas = [
+    { id: '31031685', title: 'Partita in C moll BWV 997' },
+    { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+    { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  ];
+  
+  // Adicione seu código aqui
+
+const verifySongId = musicas.find((song) => song.id === '31031685')
+// console.log(verifySongId);
