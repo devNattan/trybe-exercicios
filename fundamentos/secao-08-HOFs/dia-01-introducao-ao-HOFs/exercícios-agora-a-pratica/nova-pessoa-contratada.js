0740c1e19b48aa285a67cfbfcf340b9fa8591a47
devNattan <natanaelhenrique.s.c@gmail.com>
2022-11-30 11:59:42 -0300

    criando função que retorna objeto com nome completo e email, sendo o email uma template literal

---
+++ fundamentos/secao-08-HOFs/dia-01-introducao-ao-HOFs/exercícios-agora-a-pratica/nova-pessoa-contratada.js
@@ -0,0 +1,15 @@
+const newEmployees = (employeeGenerator) => {
+    const employees = {
+      id1: employeeGenerator('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: employeeGenerator('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: employeeGenerator('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
+
+  const employeeGenerator =  (nomeCompleto) => {
+    const email = nomeCompleto.toLowerCase().replace(' ', '_')
+    return {nomeCompleto, email: `${email}@trybe.com`};
+  };
+
+//   console.log(employeeGenerator('Pedro Guerra'));
