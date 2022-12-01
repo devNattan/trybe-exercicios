f0fee3e7cca4042afe2fee0484efdf34ab928ebf
devNattan <natanaelhenrique.s.c@gmail.com>
2022-12-01 10:25:15 -0300

    função que retorna true caso todos os autores tenham nascido no seculo XX

--- fundamentos/secao-08-HOFs/dia-01-introducao-ao-HOFs/exercícios-agora-a-pratica/parte-2.js
+++ fundamentos/secao-08-HOFs/dia-01-introducao-ao-HOFs/exercícios-agora-a-pratica/parte-2.js
@@ -81,3 +81,21 @@ const smallerName = () => {
     return nameBook;
 };
 console.log(smallerName());
+
+//Encontre o primeiro livro cujo nome possua 26 caracteres.
+const getNamedBook = () => {
+    return books.find((book) => book.name.length === 26);
+};
+console.log(getNamedBook());
+
+// Faça uma função que retorne true, se todas as pessoas autoras nasceram no século XX, ou false, caso contrário.
+const expectedResult = false;
+
+const everyoneWasBornOnSecXX = () => {
+  return books.every((book) => (
+    book.author.birthYear >= 1901 && book.author.birthYear <= 2000
+  ));
+};
+console.log(everyoneWasBornOnSecXX());
+
+// Faça uma função que retorne true, se algum livro tiver sido lançado na década de 80, e false, caso contrário.
