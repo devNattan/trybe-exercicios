5fc771afaae2f97fa1bc566d1467e9878bf77121
devNattan <natanaelhenrique.s.c@gmail.com>
2022-12-06 13:50:43 -0300

    exercicios do dia 1, parte 2

--- fundamentos/secao-08-HOFs/dia-01-introducao-ao-HOFs/exercícios-agora-a-pratica/parte-1.js
+++ fundamentos/secao-08-HOFs/dia-01-introducao-ao-HOFs/exercícios-agora-a-pratica/parte-1.js
@@ -1,3 +1,4 @@
+// nova pessoa contratada
 const newEmployees = (employeeGenerator) => {
   const employees = {
     id1: employeeGenerator('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
@@ -12,3 +13,40 @@ const employeeGenerator = (fullName) => {
   return {fullName, email: `${email}@trybe.com`};
 };
 console.log(newEmployees(employeeGenerator));
+
+// loteria
+
+const numberChecker = (myNumber, number) => myNumber === number;
+
+const lotteryResult = (myNumber, numberChecker) => {
+  const number = Math.floor((Math.random() * 5) + 1);
+
+  return numberChecker(myNumber, number) ? 'parabén você ganhou!' : 'tente novamente!'
+};
+
+console.log(lotteryResult(2, numberChecker));
+
+// analisando repostas de alunos
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compareAnswers = (rightAnswer, studentAnswer) => {
+  if (rightAnswer === studentAnswer) {
+    return 1;
+  } if (studentAnswer === 'N.A') {
+    return 0;
+  }
+  return -0.5;
+};
+
+const countPoints = (rightAnswers, studentAnswers, action) => {
+  let contador = 0;
+  for (let index = 0; index < rightAnswers.length; index += 1) {
+    const actionReturn = action(rightAnswers[index], studentAnswers[index]);
+    contador += actionReturn;
+  }
+  return `Resultado final: ${contador} pontos`;
+};
+
+console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
